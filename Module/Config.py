1ecd4673e90c4c13d7cf4431cdc97bf0218a7fe1
ZhangWH0129 <zwh20000129@gmail.com>
2026-01-12 16:31:45 +0800

    Add files via upload

---
+++ Module/Config.py
@@ -0,0 +1,55 @@
+import ml_collections
+
+
+def get_ZReg_LPBA40_config():
+    config = ml_collections.ConfigDict()
+    config.if_transskip = True
+    config.if_convskip = True
+    config.patch_size = 4
+    config.in_chans = 1
+    config.embed_dim = 96
+    config.depths = (2, 2, 4, 2)
+    config.num_heads = (4, 4, 8, 8)
+    # config.num_heads = (4, 4, 8, 8)
+    #config.window_size = (6, 6, 6)
+    config.window_size = (5, 6, 5)
+    #config.window_size = (5, 6, 7)
+    config.mlp_ratio = 4
+    config.pat_merg_rf = 4
+    config.qkv_bias = False
+    config.drop_rate = 0
+    config.drop_path_rate = 0.3
+    config.ape = False
+    config.spe = False
+    config.patch_norm = True
+    config.use_checkpoint = False
+    config.out_indices = (0, 1, 2, 3)
+    config.reg_head_chan = 16
+    config.img_size = (160, 192, 160)
+    return config
+
+def get_ZRegIXI_LPBA40_config():
+    config = ml_collections.ConfigDict()
+    config.if_transskip = True
+    config.if_convskip = True
+    config.patch_size = 4
+    config.in_chans = 1
+    config.embed_dim = 96
+    config.depths = (2, 2, 4, 2)
+    config.num_heads = (4, 4, 8, 8)
+    # config.num_heads = (4, 4, 8, 8)
+    #config.window_size = (5, 6, 5)
+    config.window_size = (5, 6, 7)
+    config.mlp_ratio = 4
+    config.pat_merg_rf = 4
+    config.qkv_bias = False
+    config.drop_rate = 0
+    config.drop_path_rate = 0.3
+    config.ape = False
+    config.spe = False
+    config.patch_norm = True
+    config.use_checkpoint = False
+    config.out_indices = (0, 1, 2, 3)
+    config.reg_head_chan = 16
+    config.img_size = (160, 192, 224)
+    return config
